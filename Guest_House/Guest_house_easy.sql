e86d8200edcdd869018983d9366108962ef5a604
Evan Hosmer <38225896+evanhosmer@users.noreply.github.com>
2019-04-22 12:40:31 -0600

    Add files via upload

--- Guest_House/Guest_house_easy.sql
+++ Guest_House/Guest_house_easy.sql
@@ -23,3 +23,31 @@ ORDER BY arrival_time
 3) Look up daily rates. Give the daily rate that should be paid for bookings with ids 5152, 5165,
 5154 and 5295. Include booking id, room type, number of occupants and the amount.
 '''
+
+SELECT DISTINCT booking_id, room_type_requested, occupants, rate.amount
+FROM booking
+JOIN rate
+ON booking.room_type_requested = rate.room_type
+WHERE booking.booking_id IN ('5152', '5165', '5154', '5295')
+
+'''
+4) Who’s in 101? Find who is staying in room 101 on 2016-12-03, include first name, last name and address.
+'''
+
+SELECT guest.first_name, guest.last_name, guest.address
+FROM guest
+JOIN booking
+ON booking.guest_id = guest.id
+WHERE booking.room_no = '101' AND booking.booking_date = '2016-12-03'
+
+'''
+5)
+How many bookings, how many nights? For guests 1185 and 1270 show the number of bookings made and
+the total number of nights. Your output should include the guest id and the total number of
+bookings and the total number of nights.
+'''
+
+SELECT guest_id, COUNT(*) as Bookings, SUM(nights) as Nights
+FROM booking
+WHERE guest_id IN ('1185','1270')
+GROUP BY guest_id
