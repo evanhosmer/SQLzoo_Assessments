f16d57444c164b6b8892d7aa6a3caee1178bd82f
Evan Hosmer <38225896+evanhosmer@users.noreply.github.com>
2019-04-22 15:01:19 -0600

    Add files via upload

--- Adventure_Works/Adv_hard.sql
+++ Adventure_Works/Adv_hard.sql
@@ -19,3 +19,41 @@ WHERE
   Address.City = 'Dallas'
 GROUP BY
   Customer.CompanyName;
+
+'''
+12) For each order show the SalesOrderID and SubTotal calculated three ways:
+A) From the SalesOrderHeader
+B) Sum of OrderQty*UnitPrice
+C) Sum of OrderQty*ListPrice
+'''
+
+SELECT SalesOrderHeader.SalesOrderID,
+SalesOrderHeader.SubTotal,
+SUM(OrderQty * UnitPrice),
+SUM(OrderQty * ListPrice)
+FROM
+SalesOrderHeader
+JOIN
+SalesOrderDetail
+ON SalesOrderHeader.SalesOrderID = SalesOrderDetail.SalesOrderID
+JOIN
+Product
+ON SalesOrderDetail.ProductID = Product.ProductID
+GROUP BY SalesOrderHeader.SalesOrderID, SalesOrderHeader.Subtotal
+
+'''
+13) Show the best selling item by value
+'''
+
+SELECT
+  Product.Name,
+  SUM(SalesOrderDetail.OrderQty * SalesOrderDetail.UnitPrice) AS Total_Sale_Value
+FROM
+  Product
+  JOIN
+    SalesOrderDetail
+    ON Product.ProductID = SalesOrderDetail.ProductID
+GROUP BY
+  Product.Name
+ORDER BY
+  Total_Sale_Value DESC;
